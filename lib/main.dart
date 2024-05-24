8a976a190547b4531a9ab1b17b8b44f4c8216e5f
KarimTamer <kemotamer448@gmail.com>
2024-05-25 02:36:12 +0300

    using get material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 void main() {
   runApp(BooklyApp());
@@ -9,7 +10,7 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
     );
   }
