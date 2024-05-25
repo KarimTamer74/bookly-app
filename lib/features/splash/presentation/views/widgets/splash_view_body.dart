74c31a29fa516b22b88f75e6574121a6f415e2b3
KarimTamer <kemotamer448@gmail.com>
2024-05-25 04:31:45 +0300

    splash ui done

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
@@ -12,11 +13,14 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(
-          'assets/images/book_logo-removebg-preview.png',
+          'assets/images/book_logo.png',
           width: 70.sp,
           height: 70.sp,
         ),
         Image.asset(AssetsData.logo),
+        Text('Read more, discover more',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.padyakkeExpandedOne()),
       ],
     );
   }
