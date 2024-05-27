b8bc161c63c7a2de2813ec5e75d0cef64d5583d0
KarimTamer <kemotamer448@gmail.com>
2024-05-27 21:44:06 +0300

    building the upper of home_body, appBar and horizontal list view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_appbar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/list_view_of_books_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -8,8 +9,17 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 25, right: 30, top: 15, bottom: 15),
-      child: ListView(
-        children: const [CustomAppBar()],
+      child: Column(
+        children: [
+          const CustomAppBar(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          const ListViewOfBooksItem(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+        ],
       ),
     );
   }
