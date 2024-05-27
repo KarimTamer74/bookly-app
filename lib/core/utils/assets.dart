5ebfb5e316fc4504085dab5ebb399e42c4fb74a0
KarimTamer <kemotamer448@gmail.com>
2024-05-27 21:42:07 +0300

    add books images and create list of them

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,16 @@
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
+
+  static const book1 = 'assets/images/book1.png';
+
+  static const book2 = 'assets/images/Book2.png';
+
+  static const book3 = 'assets/images/Book3.png';
+
+  // static const book4 = 'assets/images/Logo.png';
+  static const List<String> booksList = [
+    AssetsData.book1,
+    AssetsData.book2,
+    AssetsData.book3
+  ];
 }
