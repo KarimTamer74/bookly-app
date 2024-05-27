0ac3c180e6bb310701f9837e182692f11985a4e1
KarimTamer <kemotamer448@gmail.com>
2024-05-27 18:57:46 +0300

    add const keyword

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
 void main() {
-  runApp(BooklyApp());
+  runApp(const BooklyApp());
 }
 
 class BooklyApp extends StatelessWidget {
