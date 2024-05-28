230b4dad33ee568c6e4984679cb7c51727a642ad
KarimTamer <kemotamer448@gmail.com>
2024-05-28 18:07:11 +0300

    home body ui done

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/core/utils/stayles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_appbar.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/list_view_of_books_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/horizontal_list_view_of_books_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -9,21 +10,23 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 25, right: 30, top: 15, bottom: 15),
-      child: ListView(
-        children: [
+      padding: const EdgeInsets.only(left: 30, right: 30, top: 15, bottom: 15),
+      child: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
           const CustomAppBar(),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.05,
+          SliverToBoxAdapter(
+            child: SizedBox(height: MediaQuery.of(context).size.height * 0.05),
           ),
-          const ListViewOfBooksItem(),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.05,
+          const HorizontalListViewOfBooksItem(),
+          const SliverToBoxAdapter(child: SizedBox(height: 49)),
+          SliverToBoxAdapter(
+            child: Text('Best Seller',
+                style:
+                    Styles.textStyle18.copyWith(fontWeight: FontWeight.w600)),
           ),
-          const Text(
-            'Best Seller',
-            style: Styles.titleMedium,
-          )
+          const SliverToBoxAdapter(child: SizedBox(height: 20)),
+          const BestSellerListView()
         ],
       ),
     );
