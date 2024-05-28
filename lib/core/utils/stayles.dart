d4a0a0225e91bec2be5f58e37cb991c5e2cc24b9
KarimTamer <kemotamer448@gmail.com>
2024-05-28 14:58:05 +0300

    modifing styles class, and create textStyle for app

--- lib/core/utils/stayles.dart
+++ lib/core/utils/stayles.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const titleMedium =
-      TextStyle(fontSize: 20, fontWeight: FontWeight.w600);
+  static const textStyle14 = TextStyle(fontSize: 14); // regular font weight
+
+  static const textStyle16 =
+      TextStyle(fontSize: 16, fontWeight: FontWeight.w500); //medium font weight
+
+  static const textStyle18 = TextStyle(
+      fontSize: 18, fontWeight: FontWeight.w500); // medium font weight
+
+  static const textStyle20 = TextStyle(fontSize: 20); // regular font weight
 }
