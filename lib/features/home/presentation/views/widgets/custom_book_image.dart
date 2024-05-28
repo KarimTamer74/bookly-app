6b389cec190cea09678c034a1c95b0fa84548b29
KarimTamer <kemotamer448@gmail.com>
2024-05-28 17:55:08 +0300

    create custom book image

---
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -0,0 +1,22 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomBookImage extends StatelessWidget {
+  const CustomBookImage({super.key, required this.index, required this.aspectRatio});
+final int index;
+final double aspectRatio;
+  @override
+  Widget build(BuildContext context) {
+    return  AspectRatio(
+                aspectRatio: aspectRatio,
+                child: Container(
+                  margin: const EdgeInsets.only(right: 15),
+                  decoration: BoxDecoration(
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.circular(15),
+                      image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage(AssetsData.booksList[index]))),
+                ));
+  }
+}
