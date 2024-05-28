eb93ae7443d054c9091f7f6219a0780e1329e619
KarimTamer <kemotamer448@gmail.com>
2024-05-28 17:59:32 +0300

    modifing horizontal_list_view_of_books_item

---
+++ lib/features/home/presentation/views/widgets/horizontal_list_view_of_books_item.dart
@@ -0,0 +1,25 @@
+import 'package:bookly_app/core/utils/assets.dart';
+
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:flutter/material.dart';
+
+class HorizontalListViewOfBooksItem extends StatelessWidget {
+  const HorizontalListViewOfBooksItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .28,
+      child: ListView.builder(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemCount: AssetsData.booksList.length,
+          itemBuilder: (context, index) {
+            return CustomBookImage(
+              index: index,
+              aspectRatio: 2.8 / 4,
+            );
+          }),
+    );
+  }
+}
