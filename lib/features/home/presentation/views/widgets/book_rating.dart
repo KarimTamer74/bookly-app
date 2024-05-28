b5138204ea34d3970a7286de3cbf727431c4c615
KarimTamer <kemotamer448@gmail.com>
2024-05-28 17:55:50 +0300

    create book rating widget

---
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -0,0 +1,30 @@
+import 'package:bookly_app/core/utils/stayles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BookRating extends StatelessWidget {
+  const BookRating({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Icon(
+          Icons.star_rate_rounded,
+          size: 24,
+          color: Color(0xffFFDD4F),
+        ),
+        SizedBox(
+          width: 1.sp,
+        ),
+        const Text('4.8', style: Styles.textStyle16),
+        SizedBox(
+          width: 3.sp,
+        ),
+        Text('(2390)',
+            style: Styles.textStyle14
+                .copyWith(color: Colors.white.withOpacity(.5))),
+      ],
+    );
+  }
+}
