f0b941fe926d702aeb705f4018bffc9dc35bdcd4
KarimTamer <kemotamer448@gmail.com>
2024-08-12 20:40:17 +0300

    create api services and get books request

--- lib/features/home/data/api_services/api_services.dart
+++ lib/features/home/data/api_services/api_services.dart
@@ -1,3 +1,11 @@
-class ApiServices {
-  
+import 'package:bookly_app/core/utils/constants.dart';
+import 'package:bookly_app/features/home/data/models/book/books_response.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+part 'api_services.g.dart';
+@RestApi(baseUrl: baseUrl)
+abstract class ApiServices {
+  factory ApiServices(Dio dio, {String baseUrl}) = _ApiServices;
+  @GET('volumes?q=programming')
+  Future<BooksResponse> getBooks();
 }
